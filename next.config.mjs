f972a86c0e1b9c1d5015af7154156632beb84339
me3enov <mezenov.dizy@gmail.com>
2024-10-09 22:23:55 +0700

    Added Google Sheets integration and GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Экспорт проекта как статические файлы
+    trailingSlash: true, // Добавляет слеш к каждому URL, что нужно для корректной работы на GitHub Pages
+};
 
 export default nextConfig;
