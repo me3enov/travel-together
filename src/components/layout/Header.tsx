739680c579ec26dbcec2c421e3f6b96f8c89715b
me3enov <mezenov.dizy@gmail.com>
2024-10-10 05:54:47 +0700

    deploy

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { FC } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { openResetPopup } from '@/store/slices/popupSlice';
+import { useSelector } from 'react-redux';
 import HomeButton from '@/components/shared/HomeButton';
 import Token from '@/components/shared/Token';
 import { RootState } from '@/store';
@@ -13,15 +12,10 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ showHomeButton }) => {
   const tokens = useSelector((state: RootState) => state.token.availableTokens);
-  const dispatch = useDispatch();
-
-  const handleOpenResetPopup = () => {
-    dispatch(openResetPopup());
-  };
 
   return (
     <header className="w-full h-16 flex justify-between items-center p-4 bg-transparent fixed top-0 left-0 z-50">
-      {showHomeButton && <HomeButton onClick={handleOpenResetPopup} />}
+      {showHomeButton && <HomeButton />}
       {showHomeButton && (
         <div className="flex space-x-8">
           <Token value={1} count={tokens.first} />
