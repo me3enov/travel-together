12ac22f85cf0fe22793558f971e21e61a2480a55
me3enov <mezenov.dizy@gmail.com>
2024-10-10 07:03:50 +0700

    deploy

--- src/app/round/[round]/[selection]/page.tsx
+++ src/app/round/[round]/[selection]/page.tsx
@@ -247,3 +247,15 @@ const RoundPage = () => {
 };
 
 export default RoundPage;
+
+export async function generateStaticParams() {
+  const rounds = ['1', '2', '3', '4'];
+  const selections = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'rescue'];
+
+  return rounds.flatMap((round) =>
+    selections.map((selection) => ({
+      round,
+      selection,
+    })),
+  );
+}
